87a8f08861d0fcfd39544e740a8c51a3c0d63974
Nikki Dag <nikki.dag@temporal.io>
2024-07-10 10:32:06 -0500

    Initial commit.

---
+++ app/workflow.go
@@ -0,0 +1,45 @@
+package app
+
+import (
+	"github.com/nikki-dag/saas-nexus-bug-bash-api/service"
+	"go.temporal.io/sdk/workflow"
+)
+
+const (
+	TaskQueue    = "nexus-bug-bash-caller-task-queue"
+	endpointName = "nexus_bug_bash_endpoint"
+)
+
+func EchoCallerWorkflow(ctx workflow.Context, message string) (string, error) {
+	c := workflow.NewNexusClient(endpointName, service.BugBashServiceName)
+
+	fut := c.ExecuteOperation(ctx, service.EchoOperationName, service.EchoInput{Message: message}, workflow.NexusOperationOptions{})
+	var res string
+
+	var exec workflow.NexusOperationExecution
+	if err := fut.GetNexusOperationExecution().Get(ctx, &exec); err != nil {
+		return "", err
+	}
+	if err := fut.Get(ctx, &res); err != nil {
+		return "", err
+	}
+
+	return res, nil
+}
+
+func HelloCallerWorkflow(ctx workflow.Context, name string, language string) (string, error) {
+	c := workflow.NewNexusClient(endpointName, service.BugBashServiceName)
+
+	fut := c.ExecuteOperation(ctx, service.HelloOperationName, service.HelloInput{Name: name, Language: language}, workflow.NexusOperationOptions{})
+	var res string
+
+	var exec workflow.NexusOperationExecution
+	if err := fut.GetNexusOperationExecution().Get(ctx, &exec); err != nil {
+		return "", err
+	}
+	if err := fut.Get(ctx, &res); err != nil {
+		return "", err
+	}
+
+	return res, nil
+}
